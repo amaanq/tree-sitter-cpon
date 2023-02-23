0f84fb732194a85045a6f903c6a8c5ddbc9b1e3f
Amaan Qureshi <amaanq12@gmail.com>
2023-02-23 18:24:49 -0500

    feat: touch up bindings

---
+++ bindings/rust/README.md
@@ -0,0 +1,36 @@
+# tree-sitter-cpon
+
+This crate provides a ChainPack Object Notation (CPON) grammar for the [tree-sitter][]
+parsing library. To use this crate, add it to the `[dependencies]` section of
+your `Cargo.toml` file. (Note that you will probably also need to depend on the
+[`tree-sitter`][tree-sitter crate] crate to use the parsed result in any useful
+way.)
+
+```toml
+[dependencies]
+tree-sitter = "~0.20.3"
+tree-sitter-cpon = "0.0.1"
+```
+
+Typically, you will use the [language][language func] function to add this
+grammar to a tree-sitter [Parser][], and then use the parser to parse some code:
+
+```rust
+let code = r#"
+    fn double(x: i32) -> i32 {
+        x * 2
+    }
+"#;
+let mut parser = Parser::new();
+parser.set_language(tree_sitter_cpon::language()).expect("Error loading CPON grammar");
+let parsed = parser.parse(code, None);
+```
+
+If you have any questions, please reach out to us in the [tree-sitter
+discussions] page.
+
+[language func]: https://docs.rs/tree-sitter-cpon/*/tree_sitter_cpon/fn.language.html
+[parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
+[tree-sitter]: https://tree-sitter.github.io/
+[tree-sitter crate]: https://crates.io/crates/tree-sitter
+[tree-sitter discussions]: https://github.com/tree-sitter/tree-sitter/discussions
--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,4 +1,4 @@
-//! This crate provides json language support for the [tree-sitter][] parsing library.
+//! This crate provides ChainPack Object Notation (CPON) language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_json::language()).expect("Error loading json grammar");
+//! parser.set_language(tree_sitter_cpon::language()).expect("Error loading CPON grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,27 +18,38 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_json() -> Language;
+    fn tree_sitter_cpon() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_json() }
+    unsafe { tree_sitter_cpon() }
 }
+
+/// The source of the Rust tree-sitter grammar description.
+pub const GRAMMAR: &str = include_str!("../../grammar.js");
+
+/// The folds query for this language.
+pub const FOLDS_QUERY: &str = include_str!("../../queries/folds.scm");
+
+/// The syntax highlighting query for this language.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+
+/// The indents query for this language.
+pub const INDENTS_QUERY: &str = include_str!("../../queries/indents.scm");
+
+/// The injection query for this language.
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+
+/// The symbol tagging query for this language.
+pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
-
-// Uncomment these to include any queries that this grammar contains
-
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 #[cfg(test)]
 mod tests {
@@ -47,6 +58,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading json language");
+            .expect("Error loading CPON grammar");
     }
 }
